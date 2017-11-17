754841e1666d78e4a853cd9a83a11156937af018
Yoav Weiss <yoav@yoav.ws>
2017-11-17 18:57:28 +0100

    Added license and copyright

--- cuckoo_test.cpp
+++ cuckoo_test.cpp
@@ -1,3 +1,18 @@
+// Copyright 2017 Akamai Technologies, Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include "cuckoo.h"
 #include <fstream>
 #include <vector>
@@ -6,7 +21,9 @@ int main() {
     const unsigned MaxCount = 500;
 
     // Create a digest
-    Cuckoo cuckoo(8, 2039, MaxCount);
+    Cuckoo cuckoo(8, 1109, MaxCount);
+    //Cuckoo cuckoo(8, 2039, MaxCount);
+    //Cuckoo cuckoo(8, 4027, MaxCount);
 
     // Run a set of sanity tests
     cuckoo.runTests();
